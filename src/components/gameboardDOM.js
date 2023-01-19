d80d6e0b7602b904f6af6548dfc41c3cf544e4c7
Phlegm Mignon <dmah@ucdavis.edu>
2023-01-19 00:54:50 -0800

    Finished render of gameboard

--- src/components/gameboardDOM.js
+++ src/components/gameboardDOM.js
@@ -1,19 +1,26 @@
 function makeGameboard() {
   let grid = document.createElement("div");
 
-  for (let i = 0; i < 99; i++) {
+  for (let i = 0; i < 100; i++) {
     let tile = document.createElement("div");
     tile.id = "tile" + i;
-    tile.style.width = "15px";
-    tile.style.height = "15px";
+    tile.classList.add("tile");
+    tile.style.width = "50px";
+    tile.style.height = "50px";
     // tile.addEventListener()
 
     grid.appendChild(tile);
   }
 
   grid.id = "grid";
-  grid.style.width = "150px";
-  grid.style.height = "150px";
+  grid.style.display = "grid";
+  grid.style.justifyContent = "center";
+  grid.style.alignContent = "center";
+  grid.style.width = "625px";
+  grid.style.height = "625px";
+  grid.style.backgroundColor = "#D3D3D3";
+
+  return grid;
 }
 
 export default makeGameboard;
