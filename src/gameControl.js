abfad102ca382e84256cd5e7e9a950774314b92e
Phlegm Mignon <dmah@ucdavis.edu>
2023-01-22 00:15:32 -0800

    Added waitForUser ship inputs WIP user placing ships

--- src/gameControl.js
+++ src/gameControl.js
@@ -1,26 +1,69 @@
 import playerFactory from "./playerFactory";
 import makeGameboard from "./components/gameboardDOM";
 import { makeShipLocationsPrompt } from "./components/startPrompts";
+import gameboardFactory from "./gameboardFactory";
+// import { resolvePlugin } from "@babel/core";
 
-// let player1 = playerFactory(gameboard1, true);
-// let player2;
+async function gameControl(isHuman) {
+  let player1 = playerFactory(true);
+  let player2 = isHuman ? playerFactory(true) : playerFactory(false);
 
-let vsHuman;
-const isHuman = (boolean) => {
-  vsHuman = boolean;
-};
+  let p1Ships = [];
+  let p2Ships = [];
+  let instructions = [
+    "Place your carrier",
+    "Place your battleship",
+    "Place your cruiser",
+    "Place your submarine",
+    "Place your destoryer",
+  ];
 
-const pickShipLocations = () => {
-  let setupGameboard1 = makeGameboard();
+  let shipLocations = [];
+  let shipLengths = [5, 4, 3, 3, 2];
+
+  let gameboard = makeGameboard();
 
   let prompt = makeShipLocationsPrompt();
-  prompt.appendChild(setupGameboard1);
-};
+  let instructionsPrompt = document.getElementById("instructions");
+  prompt.appendChild(gameboard);
 
-const selectBotOrHuman = (isHuman) => {
-  if (isHuman == false) {
-    // player2 = playerFactory(gameboard2, false);
+  gameboard.addEventListener("click", (e) => {
+    console.log(e.target.id);
+    shipLocations.push(e.target.id);
+  });
+
+  setTimeout(() => {
+    if (waitForUser(shipLocations, 1)) {
+      console.log(shipLocations);
+    }
+  }, 1000);
+
+  // let gameboard1 = gameboardFactory();
+  // for (let i = 0; i < 5; i++) {
+  //   shipLocations = [];
+  //   if (waitForUser(shipLocations, shipLengths[i]) == true) {
+  //     gameboard1.placeShip(shipLocations);
+  //     console.log("hi");
+  //   }
+  // }
+
+  //   //   // console.log(shipLocations);
+}
+
+function waitForUser(shipLocations, i) {
+  function poll(resolve) {
+    if (shipLocations.length == i) {
+      resolve(true);
+    } else {
+      setTimeout(() => poll(resolve), 100);
+    }
+
+    while (shipLocations.length > i) {
+      shipLocations.pop();
+    }
   }
-};
 
-export { isHuman, pickShipLocations };
+  return new Promise(poll);
+}
+
+export { gameControl };
