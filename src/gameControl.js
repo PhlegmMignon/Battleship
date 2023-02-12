61015f069e3a664ae8753e4f0314d9d018ef0710
Phlegm Mignon <dmah@ucdavis.edu>
2023-02-12 09:25:39 -0800

    Added game turn control. Added gameControl win detection

--- src/gameControl.js
+++ src/gameControl.js
@@ -2,111 +2,39 @@ import playerFactory from "./playerFactory";
 import makeGameboardDOM from "./components/gameboardDOM";
 import { makeShipLocationsPrompt } from "./components/startPrompts";
 import gameboardFactory from "./gameboardFactory";
-import turnControl from "./turnControl";
+import {
+  turnControl,
+  checkTileConflict,
+  shipPlacementHelper,
+} from "./turnControl";
+import setupGameboard from "./setup";
 // import { resolvePlugin } from "@babel/core";
 
 async function gameControl(isHuman) {
-  let turnCounter = 1;
-  let player1 = playerFactory(true);
-  let player2 = isHuman ? playerFactory(true) : playerFactory(false);
+  let objects = await setupGameboard(isHuman);
+  let winner = await turnControl(
+    objects.player1,
+    objects.player2,
+    objects.gameboard1,
+    objects.gameboard2
+  );
 
-  let p1Ships = [];
-  let p2Ships = [];
+  if (winner == "p1") {
+    alert("p1 wins");
+  } else if (winner == "p2") {
+    alert("p2 wins");
+  }
 
-  let gameboard1 = gameboardFactory();
-  let gameboard2 = gameboardFactory();
-
-  let gameboardDOM = makeGameboardDOM();
-
-  document.addEventListener("click", (e) => {
-    if (e.target.classList.contains("tile")) {
-      turnControl(e.target.id, turnCounter, gameboard1, gameboard2);
-      turnCounter++;
-    }
-  });
-
-  let prompt = makeShipLocationsPrompt();
-  prompt.appendChild(gameboardDOM);
-
-  // let instructions = [
-  //   "Place your carrier",
-  //   "Place your battleship",
-  //   "Place your cruiser",
-  //   "Place your submarine",
-  //   "Place your destoryer",
-  // ];
-
-  // let shipLocations = [];
-  // let shipLengths = [5, 4, 3, 3, 2];
-
-  // let instructionsPrompt = document.getElementById("instructions");
-  // prompt.appendChild(gameboard);
-
-  // gameboard.addEventListener("click", (e) => {
-  //   console.log(e.target.id);
-  //   shipLocations.push(e.target.id);
-  // });
-
-  // for (let i = 0; i < 50; i++) {
-  //   setTimeout(() => {
-  //     let thing = waitForUser(shipLocations, 1);
-
-  //     if (thing == false) {
-  //       console.log(shipLocations);
-  //     }
-  //   }, 1000);
-  // }
-
-  // for (let i = 0; i < 2; i++) {
-  //   shipLocations = [];
-  //   let p = new Promise((resolve) => {
-  //     let thing = waitForUser(shipLocations, shipLengths[i]);
-  //     console.log("thing is" + thing);
-  //   });
-  //   p.then((isValid) => {
-  //     if (isValid == true) {
-  //       console.log("hi");
-  //       gameboard1.placeShip(shipLocations);
-  //     }
-  //     console.log(p);
-  //   });
-
-  //   // thing.then(gameboard1.placeShip(shipLocations));
-  // }
-
-  //   //   // console.log(shipLocations);
+  document.getElementById("prompt").style.display = "none";
 }
 
-// function promiseShipInput() {}
-
-// function waitForUser(shipLocations, i) {
-//   return new Promise((resolve, reject) => {
-//     function ensureArrayLength() {
-//       if (shipLocations.length == i) {
-//         console.log("resolved");
-//         return resolve();
-//       }
-//       setTimeout(ensureArrayLength, 2000);
-//     }
-//   });
-// }
-
-// async function waitForUser(shipLocations, i) {
-//   console.log(i);
-//   console.log(shipLocations);
-//   // function poll(resolve) {
-//   if (shipLocations.length == i) {
-//     // console.log("validity check");
-//     // validityCheck(shipLocations);
-//     return true;
-//   } else {
-//     // setTimeout(() => poll(resolve), 100);
-//     setTimeout(() => waitForUser(shipLocations, i), 5000);
+// const waitForStart = (setupFinished) => {
+//   // setTimeout(() => {
+//   if (setupFinished != undefined) {
+//     console.log(setupFinished);
+//   } else {S
+//     setTimeout(waitForStart(setupFinished), 500);
 //   }
-
-//   // }
-
-//   // return new Promise(poll);
-// }
-
+//   // }, 500);
+// };
 export { gameControl };
--- src/turnControl.js
+++ src/turnControl.js
@@ -1,14 +1,29 @@
-function turnControl(tile, counter, gameboard1, gameboard2) {
-  tile = tile.substring(4);
+function turnControl(p1, p2, gameboard1, gameboard2) {
+  let counter = 0;
+  document.addEventListener("click", (e) => {
+    let tile = e.target.id;
+    tile = tile.substring(4);
+    tile = Number(tile);
 
-  //Player1 turn
-  if (counter % 2 == 1) {
-    gameboard2.receiveAttack(tile);
-  }
+    //Player1 turn
+    if (counter % 2 == 0) {
+      console.log("p1 atks " + tile);
+      gameboard2.receiveAttack(tile);
+      counter++;
 
-  if (counter % 2 == 0) {
-    gameboard1.receiveAttack(tile);
-  }
+      if (gameboard2.allShipsSunk()) {
+        return "p1";
+      }
+    } else {
+      console.log("p2 atks " + tile);
+      gameboard1.receiveAttack(tile);
+      counter++;
+
+      if (gameboard2.allShipsSunk()) {
+        return "p2";
+      }
+    }
+  });
 }
 
 export { turnControl };
