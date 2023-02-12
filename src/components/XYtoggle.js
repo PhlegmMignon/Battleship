cdde4ac290f085d8145eee16516c2fd4cbda4faf
Phlegm Mignon <dmah@ucdavis.edu>
2023-02-12 09:21:48 -0800

    Changed XYtoggle btn styles

--- src/components/XYtoggle.js
+++ src/components/XYtoggle.js
@@ -3,6 +3,11 @@ const makeXYtoggleBtn = () => {
   btn.id = "XYtoggle";
   btn.style.width = "100px";
   btn.style.height = "40px";
+  btn.style.backgroundColor = "#D3D3D3";
+  btn.style.borderRadius = "8px";
+  btn.style.display = "inline-flex";
+  btn.style.alignItems = "center";
+  btn.style.justifyContent = "center";
   btn.value = "x";
   btn.textContent = "X-axis";
 
