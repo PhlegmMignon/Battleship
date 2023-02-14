8870c5d3a9f35bc3287d53a749ea4590e49b61fb
Phlegm Mignon <dmah@ucdavis.edu>
2023-02-14 14:34:24 -0800

    Feat: turnControl now has colors during game

--- src/turnControl.js
+++ src/turnControl.js
@@ -1,25 +1,62 @@
+import { addHitColor, addMissedColor, removeColor } from "./addTileColor";
+
 function turnControl(p1, p2, gameboard1, gameboard2) {
   let counter = 0;
+
   document.addEventListener("click", (e) => {
     let tile = e.target.id;
-    tile = tile.substring(4);
-    tile = Number(tile);
 
-    if (isNaN(tile)) return;
+    if (tile.substring(0, 4) != "tile") {
+      return;
+    } else {
+      tile = tile.substring(4);
+      tile = Number(tile);
+    }
 
     //Player1 turn
     if (counter % 2 == 0) {
+      //Hide gameboard2 colors
+      removeColor();
+
       console.log("p1 atks " + tile);
       gameboard2.receiveAttack(tile);
       counter++;
+
+      //Show gameboard1 colors
+      let shipArray = gameboard1.playerShips;
+      let hitArray = [];
+      for (let i in shipArray) {
+        let ship = shipArray[i];
+
+        for (let x in ship.hitLocations) {
+          hitArray.push(ship.hitLocations[x]);
+        }
+      }
+      console.log("hitArray " + hitArray);
+      addHitColor(hitArray);
+      addMissedColor(gameboard1.missedShots);
 
       if (gameboard2.allShipsSunk()) {
         return "p1";
       }
     } else {
+      //Hide gameboard1 colors
+      removeColor();
+
       console.log("p2 atks " + tile);
       gameboard1.receiveAttack(tile);
       counter++;
+
+      // Show gameboard2 colors
+      let shipArray = gameboard2.playerShips;
+      let hitArray = [];
+      for (let i in shipArray) {
+        for (let x in shipArray[i].hitLocations) {
+          hitArray.push(shipArray[i].hitLocations[x]);
+        }
+      }
+      addHitColor(hitArray);
+      addMissedColor(gameboard2.missedShots);
 
       if (gameboard2.allShipsSunk()) {
         return "p2";
